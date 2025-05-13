3f59a2b4d259359f8d3b1b4dbe162dd8535d1f6f
cqlyj <luoyingjie0721@gmail.com>
2025-05-13 16:49:10 +0800

    feat: add IVerifier interface for zk proof verification

---
+++ src/interfaces/IVerifier.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IVerifier {
+    function verifyProof(
+        uint[2] calldata _pA,
+        uint[2][2] calldata _pB,
+        uint[2] calldata _pC,
+        uint[5] calldata _pubSignals
+    ) external view returns (bool);
+}
